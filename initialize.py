3a4d237f29a51bf2ccce1df704f42f6702bf6e78
Ikhwanul Muslimin <ikhwan.m1996@gmail.com>
2017-11-30 22:21:42 +0700

    ubah nama fungsi

--- initialize.py
+++ initialize.py
@@ -3,6 +3,13 @@ def initialize(numIns, numCls):
 	#Inisialisasi matriks Uij
 	#untuk setiap xi, total nilai xi pada j=0 s.d. j=N adalah 1
 
+	#matU:
+	# 		| class 1 | class 2 | ... | class j
+	# x1	|	val11 |  val12  | ... |  val1j
+	# x2	|   val21 |  val22  | ... |  val2j
+	# ...	|   ...   |   ...   | ... |  ...
+	# xi 	|   vali1 |  val12  | ... |  valij
+
 	matU = []
 	for  i in range(numIns):
 		totalProb = 0 #nilai batas bawah
