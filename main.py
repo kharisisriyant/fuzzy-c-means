571f060255778600072e57e10e3708388c39c46c
kharisisriyant <kharisisriyant@gmail.com>
2017-12-03 21:36:07 +0700

    final push

--- main.py
+++ main.py
@@ -5,57 +5,57 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 
 def main():
-	################################################################
-	##################### FUZZY C-MEANS ############################
-	################################################################
+	# ################################################################
+	# ##################### FUZZY C-MEANS ############################
+	# ################################################################
 
-	numericdata = []
-	centers = []
+	# numericdata = []
+	# centers = []
 
-	# Buka file data CencucIncome
-	file = open('data/CencusIncomeNumericNormalized.data.txt', 'r')
-	reader = csv.reader(file)
+	# # Buka file data CencucIncome
+	# file = open('data/CencusIncomeNumericNormalized.data.txt', 'r')
+	# reader = csv.reader(file)
 
-	# Masukin train data
-	for row in reader:
-		for i in range(len(row)):
-			row[i] = float(row[i])
-		numericdata.append(row)
+	# # Masukin train data
+	# for row in reader:
+	# 	for i in range(len(row)):
+	# 		row[i] = float(row[i])
+	# 	numericdata.append(row)
 
-	# Initialize variable
-	n_attr = len(numericdata[0])
-	n_cluster = 2
-	m = 4
-	epsilon = 0.00001
-	isBreak = False
-	epoch = 1
+	# # Initialize variable
+	# n_attr = len(numericdata[0])
+	# n_cluster = 2
+	# m = 4
+	# epsilon = 0.0001
+	# isBreak = False
+	# epoch = 1
 
-	# Initialize matrix U
-	matU = initialize(len(numericdata), n_cluster)
+	# # Initialize matrix U
+	# matU = initialize(len(numericdata), n_cluster)
 
-	# Initialize centroid matrix
-	for i in range(n_cluster):
-		centers.append([0]*n_attr)
+	# # Initialize centroid matrix
+	# for i in range(n_cluster):
+	# 	centers.append([0]*n_attr)
 
-	while not isBreak:
-		print(epoch)
+	# while not isBreak:
+	# 	print(epoch)
 
-		# Fuzzy C Means
-		calculateCenterVector(matU, m, centers, numericdata)
-		isBreak = update_u(matU, numericdata, centers, m, epsilon)
+	# 	# Fuzzy C Means
+	# 	calculateCenterVector(matU, m, centers, numericdata)
+	# 	isBreak = update_u(matU, numericdata, centers, m, epsilon)
 
-		epoch = epoch + 1
+	# 	epoch = epoch + 1
 
-	# Save Matrix U to file
-	with open('data/CentroidFinal.data.pkl', 'wb') as f:
-		pickle.dump(centers, f)
+	# # Save Matrix U to file
+	# with open('data/CentroidFinal.data.pkl', 'wb') as f:
+	# 	pickle.dump(centers, f)
 
-	with open('data/MatUFinal.data.pkl', 'wb') as f:
-		pickle.dump(matU, f)
+	# with open('data/MatUFinal.data.pkl', 'wb') as f:
+	# 	pickle.dump(matU, f)
 
-	################################################################
-	################## FUZZY C-MEANS DONE ##########################
-	################################################################
+	# ################################################################
+	# ################## FUZZY C-MEANS DONE ##########################
+	# ################################################################
 
 	# GET Y TRAIN
 	y_train = []
@@ -81,9 +81,9 @@ def main():
 
 	for element in matU:
 		if(element[0]>element[1]):
-			y_predict.append(0)
-		else:
 			y_predict.append(1)
+		else:
+			y_predict.append(0)
 
 	print(f1_score(y_train, y_predict))
 	print(accuracy_score(y_train, y_predict))
@@ -106,14 +106,14 @@ def testData():
 
 	n_cluster = 2
 	m = 4
-	epsilon = 0.00001
+	epsilon = 0.0001
 	isBreak = False
 	epoch = 1
 
 	matU = initialize(len(numericdata), n_cluster)
 
-	with open('data/CentroidFinal.data.pkl', 'wb') as f:
-		pickle.dump(centers, f)
+	with open('data/CentroidFinal.data.pkl', 'rb') as f:
+		centers = pickle.load(f)
 
 	update_u(matU, numericdata, centers, m, epsilon)
 
@@ -125,7 +125,6 @@ def testData():
 
 	# Masukin label test data
 	for row in reader:
-		print(row)
 		temp_str = row[-1].strip()
 		if(temp_str =="<=50K"):
 			y_train.append(1)
@@ -136,9 +135,9 @@ def testData():
 
 	for element in matU:
 		if(element[0]>element[1]):
-			y_predict.append(0)
-		else:
 			y_predict.append(1)
+		else:
+			y_predict.append(0)
 
 	print(f1_score(y_train, y_predict))
 	print(accuracy_score(y_train, y_predict))
